923bb1a8c9ef57cf0547e0e5f0161d17b9cea44e
Matěj Pur <mattpur@seznam.cz>
2025-08-27 23:09:54 +0200

    finetuning

--- Clipmap/heightmap.gd
+++ Clipmap/heightmap.gd
@@ -1,7 +1,8 @@
 extends Node
-var amplitude: float = 18.0
+
 var path: String = ProjectSettings.get_setting("shader_globals/noise").value
-var image: Image = (load(path) as NoiseTexture2D).noise.get_image(512, 512)
+var amplitude: float = ProjectSettings.get_setting("shader_globals/amplitude").value
+var image: Image = (load(path) as NoiseTexture2D).noise.get_image(1024, 1024)
 var size: int = image.get_width()
 
 func get_height(x,z):
