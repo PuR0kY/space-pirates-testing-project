11d7c0e6e4aa732958bb4a5a7af2f3c1086677d4
Matěj Pur <mattpur@seznam.cz>
2025-08-27 22:50:04 +0200

    wow

--- Clipmap/heightmap.gd
+++ Clipmap/heightmap.gd
@@ -1,7 +1,7 @@
 extends Node
-var path: Dictionary = ProjectSettings.get_setting("shader_globals/noise")
-var amplitude: float = 8.0
-var image: Image = load(path["value"]).get_image()
+var amplitude: float = 18.0
+var path: String = ProjectSettings.get_setting("shader_globals/noise").value
+var image: Image = (load(path) as NoiseTexture2D).noise.get_image(512, 512)
 var size: int = image.get_width()
 
 func get_height(x,z):
